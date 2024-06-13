3a5229da1ada5b33c3a23b14be3eb6c498b71cb5
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-06-13 14:45:18 -0500

    Backend and Fronted folders has been created

---
+++ Client/README.md
@@ -0,0 +1,7 @@
+README CLIENT
+
+## Strtucture of  folders
+ * **Pages:**
+* **Page_Blogs**
+* **Components:** : This Folder containt the main Components that the Click Futuro application needs
+
---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './Sass/App.scss'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1> hello virgin app</h1>
+    </>
+  )
+}
+
+export default App
