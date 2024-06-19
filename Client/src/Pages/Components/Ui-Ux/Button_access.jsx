7f37700842b56c2ffbd231d44de3b81595b93443
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-06-19 14:55:54 -0500

    ADD:UI-UX: creation of button ui-ux component

---
+++ Client/src/Pages/Components/Ui-Ux/Button_access.jsx
@@ -0,0 +1,10 @@
+import '../../../Sass/UI-Ux/button_access.scss';
+
+export const ButtonAccess = ( { children } ) => {
+
+
+    return(
+
+        <button className= "ui_ux_button_access" > { children } </button>
+    )
+}  
