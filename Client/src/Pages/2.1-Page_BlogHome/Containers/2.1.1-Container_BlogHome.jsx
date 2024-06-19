5620b17bb5bd935d0d8193a1ea4d69e3e5dff662
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-06-19 07:17:24 -0500

    ADD: BLOG HOME PAGE: creation of layout blog home and class name added

---
+++ Client/src/Pages/2.1-Page_BlogHome/Containers/2.1.1-Container_BlogHome.jsx
@@ -0,0 +1,35 @@
+export const Container_BlogHome = () => {
+
+
+    return(
+
+        <div className="container_blogHome">
+
+            <div className="blogHome_one">
+
+                <section className="blogHome_introductionBlog">
+
+                    <h2>title blog</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </section>
+
+                <section className="blogHome_aboutblog">
+                    
+                    <section className="aboutblog_indice"></section>
+
+                    <figure className="aboutblog_author">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <img ></img>
+                    </figure>
+                </section>
+            </div>
+
+            <div className="blogHome_two">
+                <h1>title blog</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, magnam.
+                </p>
+            </div>
+        </div>
+    )
+}
