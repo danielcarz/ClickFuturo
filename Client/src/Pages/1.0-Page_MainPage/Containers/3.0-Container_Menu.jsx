b9fa0920f263bb04632d8ebe17a9d3b371f6ca7a
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-06-21 09:44:33 -0500

    ADD: MENU CONTAINER: applying styles to the menu component

--- Client/src/Pages/1.0-Page_MainPage/Containers/3.0-Container_Menu.jsx
+++ Client/src/Pages/1.0-Page_MainPage/Containers/3.0-Container_Menu.jsx
@@ -1,6 +1,9 @@
 //SASS
 import '../../../Sass/Pages/1.0-Page_MainPage/Containers/3.0-SASS-Container_Menu.scss';
 
+//UI-UX COMPONENTS
+import { ButtonAccess } from '../../Components/Ui-Ux/index.js';
+
 export const  Container_Menu = () => {
 
 
@@ -8,7 +11,26 @@ export const  Container_Menu = () => {
 
         <div className ="container_menu">
 
-            <h1> Container Menu </h1>
+            <div className = "box box-one_menu_one"> 
+                <ButtonAccess> acesso contenido 1 </ButtonAccess>
+                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+            
+            </div>
+
+            <div className = "box box-two_menu_two"> 
+                <ButtonAccess> acceso contenido 2 </ButtonAccess>
+                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+            
+            </div>
+
+            <div className = "box box-three_menu_three"> 
+                <ButtonAccess> acceso contenido 3 </ButtonAccess>
+                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+            
+            </div>
 
         </div>
     )
