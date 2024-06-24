a4c01d7abe7e93d73a039c229f5e21207fc5f5e0
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-06-24 11:52:49 -0500

    ADD:RESPONSIVE DESIGN: CONTAINER ADVERTISEMENT: applyied

--- Client/src/Pages/1.0-Page_MainPage/Containers/5.0-Container_advertisements.jsx
+++ Client/src/Pages/1.0-Page_MainPage/Containers/5.0-Container_advertisements.jsx
@@ -1,6 +1,10 @@
 //SASS
 import '../../../Sass/Pages/1.0-Page_MainPage/Containers/5.0-SASS-Container_advertisements.scss';
 
+//components
+import { ButtonAccess } from '../../Components/Ui-Ux/Button_access'; 
+
+
 export const Container_Adevertisements = () => {
 
 
@@ -8,7 +12,14 @@ export const Container_Adevertisements = () => {
 
 
         <div className= "container_advertisement" >
-            <h1> Container devertisements </h1>
+            
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, tempora.</p>
+            </div>
+
+            <ButtonAccess> button_advertisement </ButtonAccess>
+
+            
 
         </div>
     )
