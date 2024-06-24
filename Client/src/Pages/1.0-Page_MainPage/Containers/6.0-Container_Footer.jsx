04e8963751559dbe9c978de6fdf15a5a9e3019a3
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-06-24 13:01:10 -0500

    ADD:RESPONSIVE DESIGN: footer

--- Client/src/Pages/1.0-Page_MainPage/Containers/6.0-Container_Footer.jsx
+++ Client/src/Pages/1.0-Page_MainPage/Containers/6.0-Container_Footer.jsx
@@ -6,9 +6,24 @@ export const Container_Footer = () => {
 
     return (
 
-        <div className = "Container_Footer" >
+        <div className = "container_Footer" >
 
-            <h1> Container Footer </h1>
+            <div className = "wrapper_footer_one">
+
+                <section className = "box_one"> 
+                    <h4> Clic futuro </h4>
+                    <p> Lorem ipsum dolor sit amet. </p>
+                </section>
+                <section className = "box_two"> <h4>Redes sociales</h4></section>
+                
+            </div> 
+
+            <div className = "wrapper_footer_two">
+                <section className = "box_three">
+                    <hr></hr> 
+                    <h6>Copy right</h6>
+                </section>
+            </div>
 
         </div>
     )
