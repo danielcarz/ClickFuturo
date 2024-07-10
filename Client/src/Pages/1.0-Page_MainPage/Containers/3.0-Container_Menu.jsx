f0309804cdd2b59fc228606fa0c84d88fc7fd8d8
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-07-10 13:33:59 -0500

    ADD:SASS:CONTAINER MENU: applying styles and positioning to the container menu component

--- Client/src/Pages/1.0-Page_MainPage/Containers/3.0-Container_Menu.jsx
+++ Client/src/Pages/1.0-Page_MainPage/Containers/3.0-Container_Menu.jsx
@@ -1,34 +1,39 @@
-//SASS
-import '../../../Sass/Pages/1.0-Page_MainPage/Containers/3.0-SASS-Container_Menu.scss';
+import React, { useContext } from 'react';
+
+import { Contex } from '../../Components/Hooks/Contex/CreateContex.jsx';
 
 //UI-UX COMPONENTS
 import { ButtonAccess } from '../../Components/Ui-Ux/index.js';
 
+//SASS
+import '../../../Sass/Pages/1.0-Page_MainPage/Containers/3.0-SASS-Container_Menu.scss';
+
 export const  Container_Menu = () => {
 
+    const { terminal_icon } = useContext( Contex );
 
     return(
 
         <div className ="container_menu">
 
             <div className = "box box-one_menu_one"> 
-                <ButtonAccess> acesso contenido 1 </ButtonAccess>
-                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
-                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+                <ButtonAccess  className= "access_content"> <figure className='blog_icon' > <img src= { terminal_icon } alt="logo_terminal" /> </figure> <h5>Acerca de la terminal</h5> </ButtonAccess>
+                <ButtonAccess className= "access_content">  <figure className='blog_icon'> <img src="" alt="logo_terminal" /> </figure> </ButtonAccess> 
+                <ButtonAccess className= "access_content">  <figure className='blog_icon'> <img src="" alt="logo_terminal" /> </figure> </ButtonAccess> 
             
             </div>
 
             <div className = "box box-two_menu_two"> 
-                <ButtonAccess> acceso contenido 2 </ButtonAccess>
-                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
-                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+                <ButtonAccess className= "access_content">  <figure className='blog_icon'> <img src="" alt="logo_terminal" /> </figure> </ButtonAccess>
+                <ButtonAccess className= "access_content">  <figure className='blog_icon'> <img src="" alt="logo_terminal" /> </figure> </ButtonAccess> 
+                <ButtonAccess className= "access_content">  <figure className='blog_icon'> <img src="" alt="logo_terminal" /> </figure> </ButtonAccess> 
             
             </div>
 
             <div className = "box box-three_menu_three"> 
-                <ButtonAccess> acceso contenido 3 </ButtonAccess>
-                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
-                <ButtonAccess> acesso contenido 1 </ButtonAccess> 
+                <ButtonAccess className= "access_content">  <figure className='blog_icon'> <img src="" alt="logo_terminal" /> </figure> </ButtonAccess> 
+                <ButtonAccess className= "access_content">  <figure className='blog_icon'> <img src="" alt="logo_terminal" /> </figure> </ButtonAccess>
+                <ButtonAccess className= "access_content">  <figure className='blog_icon'> <img src="" alt="logo_terminal" /> </figure> </ButtonAccess> 
             
             </div>
 
