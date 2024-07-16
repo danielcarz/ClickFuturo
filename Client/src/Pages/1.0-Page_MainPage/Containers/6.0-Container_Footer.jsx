7ca11d454016bab92c714bcb9998f33faaf1adbf
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-07-16 10:08:22 -0500

    FOOTER:PHONE: styles, positioning and social media images added

--- Client/src/Pages/1.0-Page_MainPage/Containers/6.0-Container_Footer.jsx
+++ Client/src/Pages/1.0-Page_MainPage/Containers/6.0-Container_Footer.jsx
@@ -1,7 +1,16 @@
+import React, {useContext} from 'react';
+
+//CONSTEX
+import { Contex } from '../../Components/Hooks/Contex/CreateContex';
+
+
+//import { instagram_icon,  } from '../../../Pages/Components/Hooks/Contex/ProviderContex';
 //SASS
 import '../../../Sass/Pages/1.0-Page_MainPage/Containers/6.0-SASS-Container_Footer.scss';
 
 export const Container_Footer = () => {
+
+    const { instagram_icon } = useContext( Contex );
 
 
     return (
@@ -11,10 +20,18 @@ export const Container_Footer = () => {
             <div className = "wrapper_footer_one">
 
                 <section className = "box_one"> 
-                    <h4> Clic futuro </h4>
-                    <p> Lorem ipsum dolor sit amet. </p>
+                    <h4> Click futuro </h4>
+                    <p className='lema'> tu guia sobre programacion y tecnologia</p> 
                 </section>
-                <section className = "box_two"> <h4>Redes sociales</h4></section>
+
+                <section className = "box_two"> 
+
+                    <figure className='social-icons'>   
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </figure>
+                </section>
                 
             </div> 
 
